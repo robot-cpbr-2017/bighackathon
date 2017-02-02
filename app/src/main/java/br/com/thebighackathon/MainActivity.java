effd8c8d30fa473ede79c38670161a68f211ae41
EduardoAugusto2015 <eduardopetreca@gmail.com>
2017-02-02 05:33:43 -0200

    firebase json

--- app/src/main/java/br/com/thebighackathon/MainActivity.java
+++ app/src/main/java/br/com/thebighackathon/MainActivity.java
@@ -1,13 +1,25 @@
 package br.com.thebighackathon;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, ActLogin.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
